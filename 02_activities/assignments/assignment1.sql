3a590add176a93a2b248d54bdef7b79b46ea6f74
Omar Alfaqih <omar_hamdi@live.com>
2025-04-19 10:37:31 -0400

    Update assignment1.sql

--- 02_activities/assignments/assignment1.sql
+++ 02_activities/assignments/assignment1.sql
@@ -18,7 +18,7 @@ limit 10
 /* 1. Write a query that returns all customer purchases of product IDs 4 and 9. */
 -- option 1
 SELECT *
-FROM customer_purchases cp
+FROM customer_purchases
 WHERE product_id = '4' 
 OR product_id = '9'
 
